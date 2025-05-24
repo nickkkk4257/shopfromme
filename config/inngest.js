8c874683960aa9e46ca07c86aed8fddddc0c57fa
nickkkk4257 <nxgprince@gmail.com>
2025-05-24 14:44:54 +0530

    all error fix

--- config/inngest.js
+++ config/inngest.js
@@ -10,11 +10,11 @@ export const syncUserCreation = inngest.createFunction(
     {
         id:'sync-user-from-clerk'
     },
-    { event: 'clerk/user.created'}
+    { event: 'clerk/user.created'},
     async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            -id:id,
+            _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl:image_url
@@ -34,9 +34,9 @@ export const syncUserUpdation = inngest.createFunction(
     },
     { event: 'clerk/user.updated' },
     async ({event}) => {
-        onst { id, first_name, last_name, email_addresses, image_url } = event.data
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            -id:id,
+            _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl:image_url
